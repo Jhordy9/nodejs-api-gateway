56bb58e7ad99290d64d6222be40a831e2456d6d0
jhordy <jhooordy@gmail.com>
2025-01-20 23:14:57 -0300

    chore: improve template, remove console log

--- app/src/use-cases/factory/makeGetCustomer.ts
+++ app/src/use-cases/factory/makeGetCustomer.ts
@@ -3,7 +3,6 @@ import { GetCustomerUseCase } from '../getCustomerUseCase';
 
 export function makeGetCustomer(): GetCustomerUseCase {
   const tableName = process.env.CUSTOMER_TABLE_NAME as string;
-  console.log('tableName', tableName);
   const customerRepository = new CustomerRepository(tableName);
   return new GetCustomerUseCase(customerRepository);
 }
